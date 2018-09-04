58fe9de1dcedec89c62338721f12f3fd0fbb2119
L3vi47h4N <root@yourbox.com>
2018-09-04 20:29:02 +0000

    Updated setup.sh, add_domain.sh, and update_kali.sh to trim down unneeded external binary usage. Added sanity checks, etc.

--- setup/add_domain.sh
+++ setup/add_domain.sh
@@ -1,15 +1,61 @@
 #!/usr/bin/env bash
 
-#need root to make changes to nginx configs
-echo STOP NOW IF NOT RUNNING AS ROOT
-echo Enter your humble chameleon domain \(i.e. phishy.net\):
-read fulldomain
-domain=$( echo $fulldomain | cut -d. -f1)
-toplevel=$( echo $fulldomain | cut -d. -f2)
-cat ./example_nginx.conf | sed "s/HCDOMAIN/$domain/g"| sed "s/HCTOPLEVEL/$toplevel/g" > /etc/nginx/sites-available/$domain.conf
-ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/$domain.conf
-echo Creating vhosts directory for nginx logs
-mkdir -p /var/log/nginx/vhosts
-echo Wrote nginx config to /etc/nginx/sites-available/$domain.conf
-echo !!!Ensure that ssl_certificate, and ssl_certificate_key are correct! Then run:
-echo sudo service nginx restart
+if ! sudo echo &>/dev/null; then echo "Unable to use sudo. Exiting."; exit 2; fi
+
+echo "Enter your humble chameleon domain (e.g. phishy.net):"
+read -p "Domain: " fulldomain
+
+domain="${fulldomain%.*}"
+toplevel="${fulldomain##*.}"
+
+# Var definition.
+outfile="/etc/nginx/sites-available/$fulldomain.conf" # $domain is derived from arg or prompt.
+enablefile="/etc/nginx/sites-enabled/$fulldomain.conf" # ^
+certfullchain="/etc/letsencrypt/live/$fulldomain/fullchain.pem"
+certkey="/etc/letsencrypt/live/$fulldomain/privkey.pem"
+vhostlogpath="/var/log/nginx/vhosts/$fulldomain"
+
+# Check for cert files (should be updated to check for both at once down the road using an exit varible)
+if ! sudo test -f "$certfullchain"; then echo "Could not find certfullchain at $certfullchain"; echo "Create this file or change the var in this script and try again."; exit 3; fi
+if ! sudo test -f "$certkey"; then echo "Could not find certkey at $certkey"; echo "Create this file or change the var in this script and try again."; exit 4; fi
+
+# heredoc for the NGINX vhost file.
+sudo bash -c "cat >$outfile" <<EOF
+server {
+    listen       0.0.0.0:443;
+    server_name ~^(.*)\.*$domain\.$toplevel;
+    ssl                  on;
+    ssl_certificate      $certfullchain;
+    ssl_certificate_key  $certkey;
+    ssl_session_timeout  5m;
+    ssl_protocols  SSLv2 SSLv3 TLSv1;
+    ssl_ciphers  ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP;
+    ssl_prefer_server_ciphers   on;
+    access_log      $vhostlogpath/access.log;
+    error_log       $vhostlogpath/error.log;
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+        proxy_pass_request_headers on;
+#not sure if we need these...
+        proxy_set_header Host \$host;
+        proxy_ssl_session_reuse off;
+    }
+}
+#
+# Might change the redirect for simple targets that actually use HTTP... HAHAHAHA
+#
+server {
+    listen      0.0.0.0:80;
+    server_name ~^(.*)\.*$domain.$toplevel;
+    # Strict Transport Security
+    add_header Strict-Transport-Security max-age=2592000;
+    rewrite ^/.*$ https://\$host\$request_uri? permanent;
+}
+EOF
+
+sudo ln -s "$outfile" "/etc/nginx/sites-enabled/$fulldomain.conf"
+echo "Creating vhosts directory for nginx logs..."
+sudo mkdir -p "$vhostlogpath"
+if [ -f $outfile ]; then echo "Wrote nginx config to $outfile"; else echo "Could not find $outfile"; exit 5; fi
+echo "Ensure the vhost is correct and symlinked, then run: sudo systemctl restart nginx"
+echo "Thanks for playing!"
--- setup/update_kali.sh
+++ setup/update_kali.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script MUST be run as root. Please double-check the README." 
-   exit 1
-fi
-wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+wget -qO- https://archive.kali.org/archive-key.asc  | apt-key add
 apt-get update
 apt-get upgrade
