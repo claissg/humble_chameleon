77f6b0a1d7beec7c97d799dd596844dbfb1db56c
fkasler <fkasler@issgs.net>
2020-02-28 10:28:02 -0500

    Kali now supports nodejs installs

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,12 +3,10 @@ if [[ $EUID -eq 0 ]]; then
    echo "This script must NOT be run as root, but the user must be a sudoer. Please double-check the README." 
    exit 1
 fi
-if ! sudo echo &>/dev/null; then echo "Unable to use sudo. Exiting."; exit 2; fi
-
 echo "Installing Node with NPM"
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y \
 nodejs \
+npm \
 build-essential \
 nginx
 
